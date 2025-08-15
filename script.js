0962459e621343ae9df13f4614bebbf2ebb9f510
marwan alrayes <94304194+marwanalrayes@users.noreply.github.com>
2025-08-15 13:00:15 +0300

    the secound challenge in ES6

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//-------First challenge---------------------------------------
 const editor = {
     title: "my first blog",
     getUpperTitle() {
@@ -10,4 +11,31 @@ console.log(getTitle());
 
 // Write code that ensures getTitle() works as expected and logs "MY FIRST BLOG"
 //.bind(editor); in index 8
-//getTitle() in index 9
+//console.log(getTitle()) in index 9
+
+//-------Secound challenge---------------------------------------
+const formHandler = {
+    value: 'initial',
+    onChange(newValue) {
+        this.value = newValue;
+    }
+};
+function simulateInputChange(callback) {
+    callback('updated');
+}
+simulateInputChange(formHandler.onChange.bind(formHandler));
+console.log(formHandler.value);
+
+// ❓ Now log formHandler.value
+//Your Tasks:
+//1] Predict what formHandler.value will be.
+/* in this task the console.log print (undefined)*/
+
+//2] Fix the bug using either .bind() or an arrow function (your choice).
+/*simulateInputChange(formHandler.onChange.bind(formHandler)); you will put .bind(formHandler)*/
+
+//Explain which fix you used and why.
+/*i use bind in index:26 to show the function simulateInputChange
+the formHandler.onChange to arrow the callback is the this in object formHandler*/
+
+//-------Theard challenge---------------------------------------
